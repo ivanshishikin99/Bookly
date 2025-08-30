3dc16e5691ceb5ba161623b65a77c035b04991d6
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-30 10:24:24 +0300

    added lifespan to the main app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from src.utils import db_helper
 
-app = FastAPI(title="Bookly", default_response_class=ORJSONResponse)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(title="Bookly", default_response_class=ORJSONResponse, lifespan=lifespan)
 
 
 if __name__ == "__main__":
