70249891304ddad76c08b40b64c03eb0872f63df
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-30 20:41:46 +0300

    added an endpoint for creating users

--- src/api_v1/user/views.py
+++ src/api_v1/user/views.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api_v1.user.crud import create_user
+from src.api_v1.user.schemas import UserRead, UserCreate
+from src.core.models import User
+from src.utils import db_helper
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("/create", response_model=UserRead, status_code=status.HTTP_200_OK)
+async def create_user_view(user_data: UserCreate,
+                           session: AsyncSession = Depends(db_helper.session_getter)) -> User | ValueError:
+    return await create_user(user_data=user_data, session=session)
