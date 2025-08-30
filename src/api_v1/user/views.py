a9704d839b511188b767faad7e7e171a221e2a20
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-30 20:47:39 +0300

    added an endpoint for extracting users by id

--- src/api_v1/user/views.py
+++ src/api_v1/user/views.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api_v1.user.crud import create_user
+from src.api_v1.user.dependencies import get_user_by_id_dependency
 from src.api_v1.user.schemas import UserRead, UserCreate
 from src.core.models import User
 from src.utils import db_helper
@@ -13,3 +14,8 @@ router = APIRouter(prefix="/users", tags=["Users"])
 async def create_user_view(user_data: UserCreate,
                            session: AsyncSession = Depends(db_helper.session_getter)) -> User | ValueError:
     return await create_user(user_data=user_data, session=session)
+
+
+@router.get("/{user_id}", response_model=UserRead, status_code=status.HTTP_200_OK)
+async def get_user_by_id_view(user: User = Depends(get_user_by_id_dependency)) -> User | HTTPException:
+    return user
