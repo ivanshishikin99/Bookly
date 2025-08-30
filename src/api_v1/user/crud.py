a5e320f175583705a616d5869432326cd5e1b21a
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-30 20:47:20 +0300

    added a crud operation for extracting users by id

--- src/api_v1/user/crud.py
+++ src/api_v1/user/crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api_v1.user.schemas import UserCreate
@@ -12,3 +13,13 @@ async def create_user(user_data: UserCreate, session: AsyncSession) -> User | Va
     await session.commit()
     await session.refresh(user)
     return user
+
+
+async def get_user_by_id(user_id: int, session: AsyncSession) -> User | HTTPException:
+    try:
+        user = await session.get(User, user_id)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+
