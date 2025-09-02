0e6da23cf12bf61c051d7cf4a72f190e1fcba69f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-02 19:56:51 +0300

    added profile relationship

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,7 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models import Base
 from src.core.models.mixins import IdMixin, CreatedAtMixin, UpdatedAtMixin
+
+if TYPE_CHECKING:
+    from src.core.models.profile import Profile
 
 
 class User(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
@@ -10,3 +15,4 @@ class User(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
     verified: Mapped[bool] = mapped_column(nullable=False)
     role_access: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    profile: Mapped["Profile"] = relationship(back_populates="user", cascade="all, delete")
