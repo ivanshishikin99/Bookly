d7a9b6593818be57cf64b6b8311b2bde1f22b9d0
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-02 20:11:27 +0300

    welcome email is now sent upon registration

--- src/api_v1/user/views.py
+++ src/api_v1/user/views.py
@@ -5,14 +5,16 @@ from src.api_v1.user.crud import create_user
 from src.api_v1.user.dependencies import get_user_by_id_dependency
 from src.api_v1.user.schemas import UserRead, UserCreate
 from src.core.models import User
+from src.mailing import send_welcome_email
 from src.utils import db_helper
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/create", response_model=UserRead, status_code=status.HTTP_200_OK)
+@router.post("/register", response_model=UserRead, status_code=status.HTTP_200_OK)
 async def create_user_view(user_data: UserCreate,
                            session: AsyncSession = Depends(db_helper.session_getter)) -> User | ValueError:
+    send_welcome_email(username=user_data.username, email=user_data.email)
     return await create_user(user_data=user_data, session=session)
 
 
