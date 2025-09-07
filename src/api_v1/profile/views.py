8b8c5c009457432d45847be4a07b76ef9c4ad5a8
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 12:19:12 +0300

    added an endpoint for getting profile info

--- src/api_v1/profile/views.py
+++ src/api_v1/profile/views.py
@@ -1,3 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
 
-router = APIRouter(prefix="profiles", tags=["Profiles"])
+from src.api_v1.profile.schemas import ProfileRead
+from src.core.models import User, Profile
+from src.utils import db_helper
+from src.utils.auth_helpers import get_profile_by_token
+
+router = APIRouter(prefix="/profiles", tags=["Profiles"])
+
+
+@router.get("/me", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+async def get_your_profile_view(profile: Profile = Depends(get_profile_by_token)) -> Profile:
+    return profile
