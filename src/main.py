c2aabd70e51f418790a0ae36e541899660bfdbba
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 12:41:26 +0300

    added redis to the lifespan function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,12 @@ from typing import AsyncIterator
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
+from redis import asyncio as aioredis
+
+from src.core.config import settings
 from src.utils import db_helper
 
 from api_v1 import router as api_v1_router
@@ -12,6 +17,10 @@ from api_v1 import router as api_v1_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    redis = aioredis.from_url(
+        f"redis://{settings.redis_config.hostname}:{settings.redis_config.port}"
+    )
+    FastAPICache.init(RedisBackend(redis), prefix=settings.redis_config.prefix)
     yield
     await db_helper.dispose()
 
