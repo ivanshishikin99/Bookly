cab2adff67dea7655f6998cc31702d53e287cd75
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 19:37:28 +0300

    changed the get profile by id dependency to get profile by username

--- src/api_v1/profile/dependencies.py
+++ src/api_v1/profile/dependencies.py
@@ -1,14 +1,14 @@
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api_v1.profile.crud import get_profile_by_id
+from src.api_v1.profile.crud import get_profile_by_username
 from src.core.models import Profile
 from src.utils import db_helper
 
 
-async def get_profile_by_id_dependency(profile_id: int, session: AsyncSession = Depends(db_helper)) -> Profile | HTTPException:
+async def get_profile_by_id_dependency(username: str, session: AsyncSession = Depends(db_helper.session_getter)) -> Profile | HTTPException:
     try:
-        user = await get_profile_by_id(profile_id=profile_id, session=session)
+        user = await get_profile_by_username(username=username, session=session)
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
     return user
