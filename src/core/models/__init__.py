1c14240d5a979ece7b098b95d6844a84ad59288f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 20:16:39 +0300

    created an email verification token table

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,9 +2,11 @@ __all__ = ("Base",
            "User",
            "Profile",
            "Book",
+           "EmailVerificationToken",
            )
 
 from .base import Base
 from .user import User
 from .profile import Profile
 from .book import Book
+from .email_verification_token import EmailVerificationToken
