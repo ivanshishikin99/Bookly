8de5fc5050a613d0adbcf5216f67fb16f7729770
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 19:14:23 +0300

    superusers are now created within the users table

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,11 +2,9 @@ __all__ = ("Base",
            "User",
            "Profile",
            "Book",
-           "SuperUser",
            )
 
 from .base import Base
 from .user import User
 from .profile import Profile
 from .book import Book
-from .superuser import SuperUser
