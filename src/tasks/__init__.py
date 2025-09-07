4671ebe6d58fff882f4826c212eb834195be7b6e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 20:16:24 +0300

    created a send verification token celery task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,11 +1,13 @@
-__all__ = ("send_welcome_email",)
+__all__ = ("send_welcome_email",
+           "send_verification_email",
+           )
 
 import logging
 import sys
 
 from src.core.config import settings
 
-from .tasks import send_welcome_email
+from .tasks import send_welcome_email, send_verification_email
 
 if sys.argv[0] == "worker":
     logging.basicConfig(level=logging.INFO, format=settings.logging_config.log_format)
