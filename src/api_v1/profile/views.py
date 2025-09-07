70888708014af2adac18f23ebe87eef9d8512f40
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 19:37:50 +0300

    added an endpoint for viewing other users' profiles

--- src/api_v1/profile/views.py
+++ src/api_v1/profile/views.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi_cache.decorator import cache
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api_v1.profile.crud import update_profile_partial, update_profile_full
+from src.api_v1.profile.dependencies import get_profile_by_id_dependency
 from src.api_v1.profile.schemas import ProfileRead, ProfileUpdatePartial, ProfileUpdateFull
-from src.core.models import Profile
+from src.core.models import Profile, User
 from src.utils import db_helper
-from src.utils.auth_helpers import get_profile_by_token
+from src.utils.auth_helpers import get_profile_by_token, get_user_by_token
 
 router = APIRouter(prefix="/profiles", tags=["Profiles"])
 
@@ -27,3 +28,11 @@ async def update_your_profile_partial_view(profile_data: ProfileUpdatePartial, p
 async def update_your_profile_full_view(profile_data: ProfileUpdateFull, profile: Profile = Depends(get_profile_by_token),
                                         session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
     return await update_profile_full(profile=profile, profile_data=profile_data, session=session)
+
+
+@router.get("/{username}", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+async def get_profile_by_username_view(user: User = Depends(get_user_by_token),
+                                 profile: Profile = Depends(get_profile_by_id_dependency)) -> Profile | HTTPException:
+    if profile.is_public or user.role_access == "Super user":
+        return profile
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="This profile is private.")
