4e01f9085c1eb3d28d900daf94cf5c0f71e57b60
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-07 12:34:13 +0300

    added update profile full endpoint; added update profile partial endpoint;

--- src/api_v1/profile/views.py
+++ src/api_v1/profile/views.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api_v1.profile.schemas import ProfileRead
-from src.core.models import User, Profile
+from src.api_v1.profile.crud import update_profile_partial, update_profile_full
+from src.api_v1.profile.schemas import ProfileRead, ProfileUpdatePartial, ProfileUpdateFull
+from src.core.models import Profile
 from src.utils import db_helper
 from src.utils.auth_helpers import get_profile_by_token
 
@@ -11,3 +13,15 @@ router = APIRouter(prefix="/profiles", tags=["Profiles"])
 @router.get("/me", response_model=ProfileRead, status_code=status.HTTP_200_OK)
 async def get_your_profile_view(profile: Profile = Depends(get_profile_by_token)) -> Profile:
     return profile
+
+
+@router.patch("/update", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+async def update_your_profile_partial_view(profile_data: ProfileUpdatePartial, profile: Profile = Depends(get_profile_by_token),
+                                           session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
+    return await update_profile_partial(profile=profile, profile_data=profile_data, session=session)
+
+
+@router.put("/update", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+async def update_your_profile_full_view(profile_data: ProfileUpdateFull, profile: Profile = Depends(get_profile_by_token),
+                                        session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
+    return await update_profile_full(profile=profile, profile_data=profile_data, session=session)
