e83243cc495cf2acb80837867ffbd737a05a5503
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-09 19:58:15 +0300

    added a task for sending password reset token emails

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,5 +1,6 @@
 __all__ = ("send_welcome_email",
            "send_verification_email",
+           "send_password_reset_token_email",
            )
 
 import logging
@@ -7,7 +8,7 @@ import sys
 
 from src.core.config import settings
 
-from .tasks import send_welcome_email, send_verification_email
+from .tasks import send_welcome_email, send_verification_email, send_password_reset_token_email
 
 if sys.argv[0] == "worker":
     logging.basicConfig(level=logging.INFO, format=settings.logging_config.log_format)
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -5,6 +5,7 @@ from src.tasks.celery_conf import celery
 
 from src.mailing.send_welcome_mail import send_welcome_email as send
 from src.mailing.send_verification_mail import send_verification_email as send_verif
+from src.mailing.send_password_reset_token import send_password_reset_email as send_reset
 
 
 @celery.task(bind=True, max_retries=5)
@@ -21,5 +22,14 @@ def send_verification_email(self, user_id: int, user_email: str, verification_to
     try:
         log.info("Sending verification email to user with id: %s", user_id)
         return send_verif(user_email=user_email, verification_token=verification_token)
+    except:
+        self.retry()
+
+
+@celery.task(bind=True, max_retries=5)
+def send_password_reset_token_email(self, user_email: str, reset_token: UUID):
+    try:
+        log.info("Sending password reset token to user with email: %s", user_email)
+        return send_reset(user_email=user_email, reset_token=reset_token)
     except:
         self.retry()
