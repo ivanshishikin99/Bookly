fa779fb4382e07799982fad700f8a160bce9692e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-09 20:23:37 +0300

    created a books router

--- src/api_v1/__init__.py
+++ src/api_v1/__init__.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from src.api_v1.user.views import router as users_router
 from src.api_v1.profile.views import router as profiles_router
+from src.api_v1.book.views import router as books_router
 
 router = APIRouter(prefix="/api_v1")
 
 router.include_router(users_router)
 
 router.include_router(profiles_router)
+
+router.include_router(books_router)
