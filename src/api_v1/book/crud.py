56786f554cc55fffe6bf999b0e82b37cd8ff917b
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-10 18:00:16 +0300

    added a function for updating books partially; added a function for updating books fully; added a function for deleting books;

--- src/api_v1/book/crud.py
+++ src/api_v1/book/crud.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api_v1.book.schemas import BookCreate
+from src.api_v1.book.schemas import BookCreate, BookUpdatePartial, BookUpdateFull
 from src.core.models import Book, User
 
 
@@ -15,8 +15,43 @@ async def create_book(book_data: BookCreate,
                       user: User,
                       session: AsyncSession) -> Book | HTTPException:
     if not user.role_access == "Super user":
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super user are allowed to create books.")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to create books.")
     book = Book(**book_data.model_dump())
     session.add(book)
     await session.commit()
     return book
+
+
+async def update_book_partial(book_data: BookUpdatePartial,
+                              user: User,
+                              book: Book,
+                              session: AsyncSession) -> Book | HTTPException:
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to modify books.")
+    for k, v in book_data.model_dump().items():
+        if k:
+            setattr(book, k, v)
+    await session.commit()
+    return book
+
+
+async def update_book_full(book_data: BookUpdateFull,
+                           user: User,
+                           book: Book,
+                           session: AsyncSession) -> Book | HTTPException:
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super user are allowed to modify books.")
+    for k, v in book_data.model_dump().items():
+        setattr(book, k, v)
+    await session.commit()
+    return book
+
+
+async def delete_book(user: User,
+                      book: Book,
+                      session: AsyncSession):
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to delete books.")
+    await session.delete(book)
+    await session.commit()
+    return {"Book deleted successfully."}
