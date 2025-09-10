765e37a14dde2f26b4202d38b5084ac29b965c4c
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-10 18:24:24 +0300

    added a many-to-one book-author relationship

--- src/core/models/book.py
+++ src/core/models/book.py
@@ -1,11 +1,18 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models import Base
 from src.core.models.mixins import IdMixin, CreatedAtMixin, UpdatedAtMixin
+
+if TYPE_CHECKING:
+    from src.core.models.author import Author
 
 
 class Book(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
     title: Mapped[str] = mapped_column(nullable=False, index=True)
     year_of_release: Mapped[int] = mapped_column(nullable=False)
-    author: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
+    author: Mapped["Author"] = relationship(back_populates="books")
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id"), index=True)
