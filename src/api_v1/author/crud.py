d306aee9ab78ab14164abc04e18d2aadb2fe6c48
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-10 18:53:51 +0300

    added a function for updating authors fully; added a function for updating authors partially; added a function for deleting authors;

--- src/api_v1/author/crud.py
+++ src/api_v1/author/crud.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api_v1.author.schemas import AuthorCreate
+from src.api_v1.author.schemas import AuthorCreate, AuthorUpdatePartial, AuthorUpdateFull
 from src.core.models import User, Author
 
 
@@ -20,3 +20,38 @@ async def get_author_by_id(author_id: int,
                            session: AsyncSession) -> Author | None:
     author = await session.get(Author, author_id)
     return author
+
+
+async def update_author_partial(author_data: AuthorUpdatePartial,
+                                user: User,
+                                author: Author,
+                                session: AsyncSession) -> Author | HTTPException:
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to modify authors.")
+    for k, v in author_data.model_dump().items():
+        if k:
+            setattr(author, k, v)
+    await session.commit()
+    return author
+
+
+async def update_author_full(author_data: AuthorUpdateFull,
+                             user: User,
+                             author: Author,
+                             session: AsyncSession) -> Author | HTTPException:
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to modify authors.")
+    for k, v in author_data.model_dump().items():
+        setattr(author, k, v)
+    await session.commit()
+    return author
+
+
+async def delete_author(user: User,
+                        author: Author,
+                        session: AsyncSession):
+    if not user.role_access == "Super user":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only super users are allowed to delete authors.")
+    await session.delete(author)
+    await session.commit()
+    return {"Author has been deleted successfully."}
