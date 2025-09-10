dc7f2d8b9560863d783dce8eef59de63400011b2
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-10 17:39:19 +0300

    added an endpoint for getting a book by id;

--- src/api_v1/book/views.py
+++ src/api_v1/book/views.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, HTTPException
+
+from src.api_v1.book.dependencies import get_book_by_id_dependency
+from src.api_v1.book.schemas import BookRead
+from src.core.models import Book
 
 router = APIRouter(prefix="/book", tags=["Books"])
+
+
+@router.get("/{book_id}", response_model=BookRead, status_code=status.HTTP_404_NOT_FOUND)
+async def get_book_by_id_view(book: Book = Depends(get_book_by_id_dependency)) -> Book | HTTPException:
+    return book
