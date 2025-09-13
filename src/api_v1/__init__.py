4e7cf6e2f061117c1e14b4a9128997077d9d673b
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-13 11:54:31 +0300

    created a reviews router;

--- src/api_v1/__init__.py
+++ src/api_v1/__init__.py
@@ -3,6 +3,7 @@ from src.api_v1.user.views import router as users_router
 from src.api_v1.profile.views import router as profiles_router
 from src.api_v1.book.views import router as books_router
 from src.api_v1.author.views import router as authors_router
+from src.api_v1.review.views import router as reviews_router
 
 router = APIRouter(prefix="/api_v1")
 
@@ -13,3 +14,5 @@ router.include_router(profiles_router)
 router.include_router(books_router)
 
 router.include_router(authors_router)
+
+router.include_router(reviews_router)
