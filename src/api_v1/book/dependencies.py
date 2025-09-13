e07722a50555bdebe06ff9962ccfa5283e3825f4
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-13 12:34:04 +0300

    separated the get book by id dependency into schema and model retrievals;

--- src/api_v1/book/dependencies.py
+++ src/api_v1/book/dependencies.py
@@ -7,7 +7,7 @@ from src.core.models import Book
 from src.utils import db_helper
 
 
-async def get_book_by_id_dependency(book_id: int,
+async def get_book_schema_by_id_dependency(book_id: int,
                                     session: AsyncSession = Depends(db_helper.session_getter)) -> BookRead | HTTPException:
     try:
         book = await get_book_by_id(book_id=book_id,
@@ -18,3 +18,13 @@ async def get_book_by_id_dependency(book_id: int,
                     year_of_release=book.year_of_release,
                     author=book.author.full_name,
                     description=book.description)
+
+
+async def get_book_by_id_dependency(book_id: int,
+                                    session: AsyncSession = Depends(db_helper.session_getter)) -> BookRead | HTTPException:
+    try:
+        book = await get_book_by_id(book_id=book_id,
+                                    session=session)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
+    return book
