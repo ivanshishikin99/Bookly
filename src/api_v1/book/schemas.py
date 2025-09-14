f5ba25852a1dbe64d0242407423c69f78cbe9ba5
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-14 15:47:37 +0300

    added a genres field to book schemas;

--- src/api_v1/book/schemas.py
+++ src/api_v1/book/schemas.py
@@ -5,6 +5,7 @@ class BookCreate(BaseModel):
     title: str
     year_of_release: int
     author_id: int
+    genres: str
     description: str
 
 
@@ -12,13 +13,15 @@ class BookRead(BaseModel):
     title: str
     year_of_release: int
     author: str
+    genres: str
     description: str
 
 
 class BookUpdatePartial(BaseModel):
     title: str | None
     year_of_release: int | None
-    author_id: int
+    author_id: int | None
+    genres: str | None
     description: str | None
 
 
