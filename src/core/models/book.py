b739f7c10b1080a18e854f20651a1025459f6eab
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-09-14 14:02:47 +0300

    added a genres-books m2m relationship;

--- src/core/models/book.py
+++ src/core/models/book.py
@@ -9,6 +9,8 @@ from src.core.models.mixins import IdMixin, CreatedAtMixin, UpdatedAtMixin
 if TYPE_CHECKING:
     from src.core.models.author import Author
     from src.core.models.review import Review
+    from src.core.models.genre import Genre
+    from src.core.models.book_genre_association_table import BookGenreAssociationTable
 
 
 class Book(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
@@ -17,4 +19,5 @@ class Book(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
     description: Mapped[str] = mapped_column(nullable=False)
     author: Mapped["Author"] = relationship(back_populates="books", lazy="selectin")
     author_id: Mapped[int] = mapped_column(ForeignKey("author.id"), index=True)
-    reviews: Mapped["Review"] = relationship(back_populates="book", cascade="all, delete", lazy="selectin")
+    reviews: Mapped[list["Review"]] = relationship(back_populates="book", cascade="all, delete", lazy="selectin")
+    genres: Mapped[list["Genre"]] = relationship(secondary="BookGenreAssociationTable", back_populates="books", lazy="selectin")
